xI-Mx-Ix/Velthoric

refactor: Extract rendering API into separate module

vx-renderer/src/main/java/net/xmx/velthoric/renderer/VxShaderDetector.java
@@ -1,0 +1,69 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer;
+
+import java.lang.reflect.Method;
+
+/**
+ * Detects if a shaderpack (Iris) is currently active.
+ * Uses reflection to avoid hard dependencies on the mod.
+ *
+ * @author xI-Mx-Ix
+ */
+public final class VxShaderDetector {
+
+    /**
+     * Whether Iris is present on the classpath
+     */
+    private static final boolean isIrisPresent = classExists("net.irisshaders.iris.api.v0.IrisApi");
+
+    // --- Iris Reflection ---
+    private static Object irisApiInstance;
+    private static Method irisIsShaderPackInUse;
+
+    static {
+        // Setup Iris reflection
+        if (isIrisPresent) {
+            try {
+                Class<?> irisApi = Class.forName("net.irisshaders.iris.api.v0.IrisApi");
+                Method getInstanceMethod = irisApi.getDeclaredMethod("getInstance");
+                irisApiInstance = getInstanceMethod.invoke(null);
+                irisIsShaderPackInUse = irisApi.getDeclaredMethod("isShaderPackInUse");
+            } catch (Exception e) {
+                VxRConstants.LOGGER.error("Velthoric failed to reflectively setup Iris API. Shader detection may fail.", e);
+            }
+        }
+    }
+
+    /**
+     * Checks if a class exists on the classpath.
+     * @param name Fully qualified class name
+     * @return True if class exists, false otherwise
+     */
+    private static boolean classExists(String name) {
+        try {
+            Class.forName(name);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if a shaderpack is currently active.
+     * @return True if Iris is rendering with a shaderpack
+     */
+    public static boolean isShaderpackActive() {
+        try {
+            if (isIrisPresent && irisApiInstance != null && irisIsShaderPackInUse != null) {
+                return (boolean) irisIsShaderPackInUse.invoke(irisApiInstance);
+            }
+        } catch (Exception e) {
+            VxRConstants.LOGGER.error("Error checking Iris shaderpack state.", e);
+        }
+
+        return false;
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/listener/VxReloadListener.java
@@ -1,0 +1,29 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.listener;
+
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.xmx.velthoric.renderer.model.VxModelManager;
+
+/**
+ * A resource reload listener that clears the {@link VxModelManager}.
+ * This ensures that models are re-parsed and re-uploaded to the GPU when
+ * resource packs are changed, preventing stale data from being rendered.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxReloadListener implements ResourceManagerReloadListener {
+
+    /**
+     * Called when the resource manager is reloaded. This method triggers
+     * the clearing of the GPU model cache.
+     * @param resourceManager The resource manager that was reloaded.
+     */
+    @Override
+    public void onResourceManagerReload(ResourceManager resourceManager) {
+        VxModelManager.clear();
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/mesh/VxVertexLayout.java
@@ -1,0 +1,70 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.mesh;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
+
+/**
+ * A utility class that defines the standard vertex layout for all meshes in the system.
+ * This centralizes the vertex attribute configuration to ensure consistency and
+ * avoid code duplication between different mesh storage strategies (e.g., standalone vs. arena).
+ *
+ * @author xI-Mx-Ix
+ */
+public final class VxVertexLayout {
+
+    /**
+     * The size of a single vertex in bytes.
+     * <ul>
+     *   <li>Position (vec3): 3 * 4 = 12 bytes</li>
+     *   <li>Color (4 ubytes): 4 * 1 = 4 bytes</li>
+     *   <li>UV0 (vec2): 2 * 4 = 8 bytes</li>
+     *   <li>UV2 (2 shorts): 2 * 2 = 4 bytes</li>
+     *   <li>Normal (3 bytes): 3 * 1 = 3 bytes</li>
+     *   <li>Tangent (3 bytes): 3 * 1 = 3 bytes</li>
+     *   <li>Total: 36 bytes</li>
+     * </ul>
+     */
+    public static final int STRIDE = 36;
+
+    // Vertex attribute locations, matching the shader bindings.
+    public static final int AT_POSITION = 0;
+    public static final int AT_COLOR = 1;
+    public static final int AT_UV0 = 2;
+    public static final int AT_UV2 = 4;
+    public static final int AT_NORMAL = 5;
+    public static final int AT_TANGENT = 13;
+
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private VxVertexLayout() {}
+
+    /**
+     * Configures the vertex attribute pointers for the currently bound Vertex Array Object (VAO).
+     * This method defines the memory layout for all vertices, specifying how the GPU should
+     * interpret the raw data from a Vertex Buffer Object (VBO).
+     */
+    public static void setupVertexAttributes() {
+        GL30.glEnableVertexAttribArray(AT_POSITION);
+        GL30.glVertexAttribPointer(AT_POSITION, 3, GL11.GL_FLOAT, false, STRIDE, 0);
+
+        GL30.glEnableVertexAttribArray(AT_COLOR);
+        GL30.glVertexAttribPointer(AT_COLOR, 4, GL11.GL_UNSIGNED_BYTE, true, STRIDE, 12);
+
+        GL30.glEnableVertexAttribArray(AT_UV0);
+        GL30.glVertexAttribPointer(AT_UV0, 2, GL11.GL_FLOAT, false, STRIDE, 16);
+
+        GL30.glEnableVertexAttribArray(AT_UV2);
+        GL30.glVertexAttribIPointer(AT_UV2, 2, GL11.GL_SHORT, STRIDE, 24);
+
+        GL30.glEnableVertexAttribArray(AT_NORMAL);
+        GL30.glVertexAttribPointer(AT_NORMAL, 3, GL11.GL_BYTE, true, STRIDE, 28);
+
+        GL30.glEnableVertexAttribArray(AT_TANGENT);
+        GL30.glVertexAttribPointer(AT_TANGENT, 3, GL11.GL_BYTE, true, STRIDE, 31);
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/mixin/impl/MixinMinecraft.java
@@ -1,0 +1,46 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.mixin.impl;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.packs.resources.ReloadableResourceManager;
+import net.xmx.velthoric.renderer.listener.VxReloadListener;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Mixin to inject into the Minecraft class.
+ * This is used to register the VxReloadListener directly into the game's
+ * resource manager at the end of the client's initialization.
+ *
+ * @author xI-Mx-Ix
+ */
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+    /**
+     * Shadows the resourceManager field from the Minecraft class.
+     * This provides direct access to the manager instance where listeners are registered.
+     */
+    @Shadow @Final private ReloadableResourceManager resourceManager;
+
+    /**
+     * Injects a method call at the end of the Minecraft constructor.
+     * This is the ideal point to add our resource listener, as all necessary
+     * game systems, including the resource manager, have been initialized.
+     *
+     * @param ci The CallbackInfo object provided by Mixin, required for the injection.
+     */
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        // Register our custom reload listener to the game's resource manager.
+        // This ensures the VxModelCache is cleared on every resource reload.
+        this.resourceManager.registerReloadListener(new VxReloadListener());
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/model/VxModelPartMapper.java
@@ -1,0 +1,55 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * A generic utility for mapping logical game components (Keys) to internal model group names (Values).
+ *
+ * <p>This class allows the rendering logic to remain decoupled from specific game mechanics.
+ * Game entities can define their own identifying Enums or constants and map them
+ * to the arbitrary string names found in 3D model files.</p>
+ *
+ * @param <K> The type of the key used to identify parts (e.g., a VehiclePart enum).
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxModelPartMapper<K> {
+
+    private final Map<K, String> mapping = new HashMap<>();
+
+    /**
+     * Maps a logical identifier to a specific group name in the model file.
+     *
+     * @param key The logical identifier (e.g., MyCarParts.WHEEL_FL).
+     * @param modelGroupName The exact name of the group in the .obj file (e.g., "obj_wheel_01").
+     */
+    public void mapGroup(K key, String modelGroupName) {
+        mapping.put(key, modelGroupName);
+    }
+
+    /**
+     * Retrieves the model group name associated with the given logical identifier.
+     *
+     * @param key The logical identifier.
+     * @return An Optional containing the model group name if mapped, or empty.
+     */
+    public Optional<String> getGroupName(K key) {
+        return Optional.ofNullable(mapping.get(key));
+    }
+
+    /**
+     * Checks if a mapping exists for the given key.
+     *
+     * @param key The key to check.
+     * @return True if a mapping exists.
+     */
+    public boolean hasMapping(K key) {
+        return mapping.containsKey(key);
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/model/generator/VxNormalGenerator.java
@@ -1,0 +1,63 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.model.generator;
+
+import it.unimi.dsi.fastutil.floats.FloatArrayList;
+import org.joml.Vector3f;
+
+/**
+ * Utility class for calculating vertex normals for 3D models.
+ *
+ * @author xI-Mx-Ix
+ */
+public final class VxNormalGenerator {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private VxNormalGenerator() {}
+
+    /**
+     * Calculates the normal vector of a face defined by three vertices.
+     * The normal is computed using the cross product of two edges of the face.
+     * This is essential for achieving correct lighting on models that do not have pre-computed normals.
+     *
+     * @param positions A list of vertex positions (x, y, z).
+     * @param idx0 The index of the first vertex of the face.
+     * @param idx1 The index of the second vertex of the face.
+     * @param idx2 The index of the third vertex of the face.
+     * @return A normalized {@link Vector3f} representing the face normal. Returns a default up-vector if the face is degenerate.
+     */
+    public static Vector3f calculateFaceNormal(FloatArrayList positions, int idx0, int idx1, int idx2) {
+        Vector3f p0 = new Vector3f(
+            positions.getFloat(idx0 * 3),
+            positions.getFloat(idx0 * 3 + 1),
+            positions.getFloat(idx0 * 3 + 2)
+        );
+        Vector3f p1 = new Vector3f(
+            positions.getFloat(idx1 * 3),
+            positions.getFloat(idx1 * 3 + 1),
+            positions.getFloat(idx1 * 3 + 2)
+        );
+        Vector3f p2 = new Vector3f(
+            positions.getFloat(idx2 * 3),
+            positions.getFloat(idx2 * 3 + 1),
+            positions.getFloat(idx2 * 3 + 2)
+        );
+
+        Vector3f edge1 = p1.sub(p0, new Vector3f());
+        Vector3f edge2 = p2.sub(p0, new Vector3f());
+        Vector3f normal = edge1.cross(edge2, new Vector3f());
+
+        if (normal.lengthSquared() > 1e-6f) {
+            normal.normalize();
+        } else {
+            // The face is degenerate (vertices are collinear), provide a fallback normal.
+            normal.set(0.0f, 1.0f, 0.0f);
+        }
+
+        return normal;
+    }
+}

xI-Mx-Ix/VelGFX

common/src/main/java/net/xmx/velgfx/renderer/VxShaderDetector.java
@@ -1,0 +1,69 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer;
+
+import java.lang.reflect.Method;
+
+/**
+ * Detects if a shaderpack (Iris) is currently active.
+ * Uses reflection to avoid hard dependencies on the mod.
+ *
+ * @author xI-Mx-Ix
+ */
+public final class VxShaderDetector {
+
+    /**
+     * Whether Iris is present on the classpath
+     */
+    private static final boolean isIrisPresent = classExists("net.irisshaders.iris.api.v0.IrisApi");
+
+    // --- Iris Reflection ---
+    private static Object irisApiInstance;
+    private static Method irisIsShaderPackInUse;
+
+    static {
+        // Setup Iris reflection
+        if (isIrisPresent) {
+            try {
+                Class<?> irisApi = Class.forName("net.irisshaders.iris.api.v0.IrisApi");
+                Method getInstanceMethod = irisApi.getDeclaredMethod("getInstance");
+                irisApiInstance = getInstanceMethod.invoke(null);
+                irisIsShaderPackInUse = irisApi.getDeclaredMethod("isShaderPackInUse");
+            } catch (Exception e) {
+                VelGFX.LOGGER.error("VelGFX failed to reflectively setup Iris API. Shader detection may fail.", e);
+            }
+        }
+    }
+
+    /**
+     * Checks if a class exists on the classpath.
+     * @param name Fully qualified class name
+     * @return True if class exists, false otherwise
+     */
+    private static boolean classExists(String name) {
+        try {
+            Class.forName(name);
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if a shaderpack is currently active.
+     * @return True if Iris is rendering with a shaderpack
+     */
+    public static boolean isShaderpackActive() {
+        try {
+            if (isIrisPresent && irisApiInstance != null && irisIsShaderPackInUse != null) {
+                return (boolean) irisIsShaderPackInUse.invoke(irisApiInstance);
+            }
+        } catch (Exception e) {
+            VelGFX.LOGGER.error("Error checking Iris shaderpack state.", e);
+        }
+
+        return false;
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/listener/VxReloadListener.java
@@ -1,0 +1,29 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.listener;
+
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
+import net.xmx.velgfx.renderer.model.VxModelManager;
+
+/**
+ * A resource reload listener that clears the {@link VxModelManager}.
+ * This ensures that models are re-parsed and re-uploaded to the GPU when
+ * resource packs are changed, preventing stale data from being rendered.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxReloadListener implements ResourceManagerReloadListener {
+
+    /**
+     * Called when the resource manager is reloaded. This method triggers
+     * the clearing of the GPU model cache.
+     * @param resourceManager The resource manager that was reloaded.
+     */
+    @Override
+    public void onResourceManagerReload(ResourceManager resourceManager) {
+        VxModelManager.clear();
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/mesh/VxVertexLayout.java
@@ -1,0 +1,70 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.mesh;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
+
+/**
+ * A utility class that defines the standard vertex layout for all meshes in the system.
+ * This centralizes the vertex attribute configuration to ensure consistency and
+ * avoid code duplication between different mesh storage strategies (e.g., standalone vs. arena).
+ *
+ * @author xI-Mx-Ix
+ */
+public final class VxVertexLayout {
+
+    /**
+     * The size of a single vertex in bytes.
+     * <ul>
+     *   <li>Position (vec3): 3 * 4 = 12 bytes</li>
+     *   <li>Color (4 ubytes): 4 * 1 = 4 bytes</li>
+     *   <li>UV0 (vec2): 2 * 4 = 8 bytes</li>
+     *   <li>UV2 (2 shorts): 2 * 2 = 4 bytes</li>
+     *   <li>Normal (3 bytes): 3 * 1 = 3 bytes</li>
+     *   <li>Tangent (3 bytes): 3 * 1 = 3 bytes</li>
+     *   <li>Total: 36 bytes</li>
+     * </ul>
+     */
+    public static final int STRIDE = 36;
+
+    // Vertex attribute locations, matching the shader bindings.
+    public static final int AT_POSITION = 0;
+    public static final int AT_COLOR = 1;
+    public static final int AT_UV0 = 2;
+    public static final int AT_UV2 = 4;
+    public static final int AT_NORMAL = 5;
+    public static final int AT_TANGENT = 13;
+
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private VxVertexLayout() {}
+
+    /**
+     * Configures the vertex attribute pointers for the currently bound Vertex Array Object (VAO).
+     * This method defines the memory layout for all vertices, specifying how the GPU should
+     * interpret the raw data from a Vertex Buffer Object (VBO).
+     */
+    public static void setupVertexAttributes() {
+        GL30.glEnableVertexAttribArray(AT_POSITION);
+        GL30.glVertexAttribPointer(AT_POSITION, 3, GL11.GL_FLOAT, false, STRIDE, 0);
+
+        GL30.glEnableVertexAttribArray(AT_COLOR);
+        GL30.glVertexAttribPointer(AT_COLOR, 4, GL11.GL_UNSIGNED_BYTE, true, STRIDE, 12);
+
+        GL30.glEnableVertexAttribArray(AT_UV0);
+        GL30.glVertexAttribPointer(AT_UV0, 2, GL11.GL_FLOAT, false, STRIDE, 16);
+
+        GL30.glEnableVertexAttribArray(AT_UV2);
+        GL30.glVertexAttribIPointer(AT_UV2, 2, GL11.GL_SHORT, STRIDE, 24);
+
+        GL30.glEnableVertexAttribArray(AT_NORMAL);
+        GL30.glVertexAttribPointer(AT_NORMAL, 3, GL11.GL_BYTE, true, STRIDE, 28);
+
+        GL30.glEnableVertexAttribArray(AT_TANGENT);
+        GL30.glVertexAttribPointer(AT_TANGENT, 3, GL11.GL_BYTE, true, STRIDE, 31);
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/mixin/impl/MixinMinecraft.java
@@ -1,0 +1,46 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.mixin.impl;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.packs.resources.ReloadableResourceManager;
+import net.xmx.velgfx.renderer.listener.VxReloadListener;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Mixin to inject into the Minecraft class.
+ * This is used to register the VxReloadListener directly into the game's
+ * resource manager at the end of the client's initialization.
+ *
+ * @author xI-Mx-Ix
+ */
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+    /**
+     * Shadows the resourceManager field from the Minecraft class.
+     * This provides direct access to the manager instance where listeners are registered.
+     */
+    @Shadow @Final private ReloadableResourceManager resourceManager;
+
+    /**
+     * Injects a method call at the end of the Minecraft constructor.
+     * This is the ideal point to add our resource listener, as all necessary
+     * game systems, including the resource manager, have been initialized.
+     *
+     * @param ci The CallbackInfo object provided by Mixin, required for the injection.
+     */
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void onInit(CallbackInfo ci) {
+        // Register our custom reload listener to the game's resource manager.
+        // This ensures the VxModelCache is cleared on every resource reload.
+        this.resourceManager.registerReloadListener(new VxReloadListener());
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/model/VxModelPartMapper.java
@@ -1,0 +1,55 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * A generic utility for mapping logical game components (Keys) to internal model group names (Values).
+ *
+ * <p>This class allows the rendering logic to remain decoupled from specific game mechanics.
+ * Game entities can define their own identifying Enums or constants and map them
+ * to the arbitrary string names found in 3D model files.</p>
+ *
+ * @param <K> The type of the key used to identify parts (e.g., a VehiclePart enum).
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxModelPartMapper<K> {
+
+    private final Map<K, String> mapping = new HashMap<>();
+
+    /**
+     * Maps a logical identifier to a specific group name in the model file.
+     *
+     * @param key The logical identifier (e.g., MyCarParts.WHEEL_FL).
+     * @param modelGroupName The exact name of the group in the .obj file (e.g., "obj_wheel_01").
+     */
+    public void mapGroup(K key, String modelGroupName) {
+        mapping.put(key, modelGroupName);
+    }
+
+    /**
+     * Retrieves the model group name associated with the given logical identifier.
+     *
+     * @param key The logical identifier.
+     * @return An Optional containing the model group name if mapped, or empty.
+     */
+    public Optional<String> getGroupName(K key) {
+        return Optional.ofNullable(mapping.get(key));
+    }
+
+    /**
+     * Checks if a mapping exists for the given key.
+     *
+     * @param key The key to check.
+     * @return True if a mapping exists.
+     */
+    public boolean hasMapping(K key) {
+        return mapping.containsKey(key);
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/model/generator/VxNormalGenerator.java
@@ -1,0 +1,63 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.model.generator;
+
+import it.unimi.dsi.fastutil.floats.FloatArrayList;
+import org.joml.Vector3f;
+
+/**
+ * Utility class for calculating vertex normals for 3D models.
+ *
+ * @author xI-Mx-Ix
+ */
+public final class VxNormalGenerator {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private VxNormalGenerator() {}
+
+    /**
+     * Calculates the normal vector of a face defined by three vertices.
+     * The normal is computed using the cross product of two edges of the face.
+     * This is essential for achieving correct lighting on models that do not have pre-computed normals.
+     *
+     * @param positions A list of vertex positions (x, y, z).
+     * @param idx0 The index of the first vertex of the face.
+     * @param idx1 The index of the second vertex of the face.
+     * @param idx2 The index of the third vertex of the face.
+     * @return A normalized {@link Vector3f} representing the face normal. Returns a default up-vector if the face is degenerate.
+     */
+    public static Vector3f calculateFaceNormal(FloatArrayList positions, int idx0, int idx1, int idx2) {
+        Vector3f p0 = new Vector3f(
+            positions.getFloat(idx0 * 3),
+            positions.getFloat(idx0 * 3 + 1),
+            positions.getFloat(idx0 * 3 + 2)
+        );
+        Vector3f p1 = new Vector3f(
+            positions.getFloat(idx1 * 3),
+            positions.getFloat(idx1 * 3 + 1),
+            positions.getFloat(idx1 * 3 + 2)
+        );
+        Vector3f p2 = new Vector3f(
+            positions.getFloat(idx2 * 3),
+            positions.getFloat(idx2 * 3 + 1),
+            positions.getFloat(idx2 * 3 + 2)
+        );
+
+        Vector3f edge1 = p1.sub(p0, new Vector3f());
+        Vector3f edge2 = p2.sub(p0, new Vector3f());
+        Vector3f normal = edge1.cross(edge2, new Vector3f());
+
+        if (normal.lengthSquared() > 1e-6f) {
+            normal.normalize();
+        } else {
+            // The face is degenerate (vertices are collinear), provide a fallback normal.
+            normal.set(0.0f, 1.0f, 0.0f);
+        }
+
+        return normal;
+    }
+}