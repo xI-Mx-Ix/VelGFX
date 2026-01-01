xI-Mx-Ix/Velthoric

refactor: Extract VxGlState into its own top-level class

vx-renderer/src/main/java/net/xmx/velthoric/renderer/gl/VxGlState.java
@@ -1,0 +1,50 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.gl;
+
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL30;
+
+/**
+ * A private utility class for safely managing and isolating critical OpenGL state.
+ * <p>
+ * This class provides a simple mechanism to save the current bindings for Vertex Array Objects (VAO),
+ * Vertex Buffer Objects (VBO), and Element Buffer Objects (EBO) before custom rendering operations
+ * and restore them afterward.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxGlState {
+    private static int previousVaoId = -1;
+    private static int previousVboId = -1;
+    private static int previousEboId = -1;
+
+    /**
+     * Queries and stores the currently bound VAO, VBO, and EBO.
+     */
+    public static void saveCurrentState() {
+        previousVaoId = GL30.glGetInteger(GL30.GL_VERTEX_ARRAY_BINDING);
+        previousVboId = GL15.glGetInteger(GL15.GL_ARRAY_BUFFER_BINDING);
+        previousEboId = GL15.glGetInteger(GL15.GL_ELEMENT_ARRAY_BUFFER_BINDING);
+    }
+
+    /**
+     * Restores the VAO, VBO, and EBO that were saved by the last call to {@link #saveCurrentState()}.
+     */
+    public static void restorePreviousState() {
+        if (previousVaoId != -1) {
+            GL30.glBindVertexArray(previousVaoId);
+            previousVaoId = -1;
+        }
+        if (previousVboId != -1) {
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, previousVboId);
+            previousVboId = -1;
+        }
+        if (previousEboId != -1) {
+            GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, previousEboId);
+            previousEboId = -1;
+        }
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/mesh/VxRenderQueue.java
@@ -14,12 +14,12 @@ import net.minecraft.client.renderer.ShaderInstance;
 import net.xmx.velthoric.renderer.VxDrawCommand;
 import net.xmx.velthoric.renderer.VxRConstants;
 import net.xmx.velthoric.renderer.VxShaderDetector;
+import net.xmx.velthoric.renderer.gl.VxGlState;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL30;
 
 import java.nio.FloatBuffer;
@@ -432,45 +432,5 @@ public class VxRenderQueue {
 
         RenderSystem.setupShaderLights(shader);
         return shader;
-    }
-
-    /**
-     * A private utility class for safely managing and isolating critical OpenGL state.
-     * <p>
-     * This class provides a simple mechanism to save the current bindings for Vertex Array Objects (VAO),
-     * Vertex Buffer Objects (VBO), and Element Buffer Objects (EBO) before custom rendering operations
-     * and restore them afterward.
-     */
-    private static class VxGlState {
-        private static int previousVaoId = -1;
-        private static int previousVboId = -1;
-        private static int previousEboId = -1;
-
-        /**
-         * Queries and stores the currently bound VAO, VBO, and EBO.
-         */
-        public static void saveCurrentState() {
-            previousVaoId = GL30.glGetInteger(GL30.GL_VERTEX_ARRAY_BINDING);
-            previousVboId = GL15.glGetInteger(GL15.GL_ARRAY_BUFFER_BINDING);
-            previousEboId = GL15.glGetInteger(GL15.GL_ELEMENT_ARRAY_BUFFER_BINDING);
-        }
-
-        /**
-         * Restores the VAO, VBO, and EBO that were saved by the last call to {@link #saveCurrentState()}.
-         */
-        public static void restorePreviousState() {
-            if (previousVaoId != -1) {
-                GL30.glBindVertexArray(previousVaoId);
-                previousVaoId = -1;
-            }
-            if (previousVboId != -1) {
-                GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, previousVboId);
-                previousVboId = -1;
-            }
-            if (previousEboId != -1) {
-                GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, previousEboId);
-                previousEboId = -1;
-            }
-        }
     }
 }

xI-Mx-Ix/VelGFX

common/src/main/java/net/xmx/velgfx/renderer/gl/VxGlState.java
@@ -1,0 +1,50 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.gl;
+
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL30;
+
+/**
+ * A private utility class for safely managing and isolating critical OpenGL state.
+ * <p>
+ * This class provides a simple mechanism to save the current bindings for Vertex Array Objects (VAO),
+ * Vertex Buffer Objects (VBO), and Element Buffer Objects (EBO) before custom rendering operations
+ * and restore them afterward.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxGlState {
+    private static int previousVaoId = -1;
+    private static int previousVboId = -1;
+    private static int previousEboId = -1;
+
+    /**
+     * Queries and stores the currently bound VAO, VBO, and EBO.
+     */
+    public static void saveCurrentState() {
+        previousVaoId = GL30.glGetInteger(GL30.GL_VERTEX_ARRAY_BINDING);
+        previousVboId = GL15.glGetInteger(GL15.GL_ARRAY_BUFFER_BINDING);
+        previousEboId = GL15.glGetInteger(GL15.GL_ELEMENT_ARRAY_BUFFER_BINDING);
+    }
+
+    /**
+     * Restores the VAO, VBO, and EBO that were saved by the last call to {@link #saveCurrentState()}.
+     */
+    public static void restorePreviousState() {
+        if (previousVaoId != -1) {
+            GL30.glBindVertexArray(previousVaoId);
+            previousVaoId = -1;
+        }
+        if (previousVboId != -1) {
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, previousVboId);
+            previousVboId = -1;
+        }
+        if (previousEboId != -1) {
+            GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, previousEboId);
+            previousEboId = -1;
+        }
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/mesh/VxRenderQueue.java
@@ -14,12 +14,12 @@ import net.minecraft.client.renderer.ShaderInstance;
 import net.xmx.velgfx.renderer.VxDrawCommand;
 import net.xmx.velgfx.renderer.VelGFX;
 import net.xmx.velgfx.renderer.VxShaderDetector;
+import net.xmx.velgfx.renderer.gl.VxGlState;
 import org.joml.Matrix3f;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL30;
 
 import java.nio.FloatBuffer;
@@ -432,45 +432,5 @@ public class VxRenderQueue {
 
         RenderSystem.setupShaderLights(shader);
         return shader;
-    }
-
-    /**
-     * A private utility class for safely managing and isolating critical OpenGL state.
-     * <p>
-     * This class provides a simple mechanism to save the current bindings for Vertex Array Objects (VAO),
-     * Vertex Buffer Objects (VBO), and Element Buffer Objects (EBO) before custom rendering operations
-     * and restore them afterward.
-     */
-    private static class VxGlState {
-        private static int previousVaoId = -1;
-        private static int previousVboId = -1;
-        private static int previousEboId = -1;
-
-        /**
-         * Queries and stores the currently bound VAO, VBO, and EBO.
-         */
-        public static void saveCurrentState() {
-            previousVaoId = GL30.glGetInteger(GL30.GL_VERTEX_ARRAY_BINDING);
-            previousVboId = GL15.glGetInteger(GL15.GL_ARRAY_BUFFER_BINDING);
-            previousEboId = GL15.glGetInteger(GL15.GL_ELEMENT_ARRAY_BUFFER_BINDING);
-        }
-
-        /**
-         * Restores the VAO, VBO, and EBO that were saved by the last call to {@link #saveCurrentState()}.
-         */
-        public static void restorePreviousState() {
-            if (previousVaoId != -1) {
-                GL30.glBindVertexArray(previousVaoId);
-                previousVaoId = -1;
-            }
-            if (previousVboId != -1) {
-                GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, previousVboId);
-                previousVboId = -1;
-            }
-            if (previousEboId != -1) {
-                GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, previousEboId);
-                previousEboId = -1;
-            }
-        }
     }
 }