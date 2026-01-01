xI-Mx-Ix/Velthoric

refactor: Implement multi-stage model pipeline with SoA-based raw data storage

vx-renderer/src/main/java/net/xmx/velthoric/renderer/model/VxModelBaker.java
@@ -1,0 +1,140 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.model;
+
+import net.xmx.velthoric.renderer.gl.VxDrawCommand;
+import net.xmx.velthoric.renderer.gl.VxMaterial;
+import net.xmx.velthoric.renderer.mesh.VxMeshDefinition;
+import net.xmx.velthoric.renderer.model.generator.VxNormalGenerator;
+import net.xmx.velthoric.renderer.model.generator.VxTangentGenerator;
+import net.xmx.velthoric.renderer.model.raw.VxRawGroup;
+import net.xmx.velthoric.renderer.model.raw.VxRawMesh;
+import net.xmx.velthoric.renderer.model.raw.VxRawModel;
+import org.joml.Vector3f;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Responsible for converting a {@link VxRawModel} (CPU-side editable data) into a
+ * {@link VxMeshDefinition} (GPU-ready interleaved buffer).
+ * <p>
+ * This class reads the optimized flat index arrays from {@link VxRawMesh} and processes
+ * them into a stream of vertices for OpenGL.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxModelBaker {
+
+    /**
+     * Bakes the raw model into a renderable mesh definition.
+     *
+     * @param rawModel The raw model data.
+     * @return The GPU-ready mesh definition.
+     */
+    public static VxMeshDefinition bake(VxRawModel rawModel) {
+        int totalVertices = rawModel.calculateTotalVertexCount();
+        if (totalVertices == 0) {
+            return new VxMeshDefinition(ByteBuffer.allocate(0), new ArrayList<>(), new HashMap<>());
+        }
+
+        // Allocate the final buffer
+        ByteBuffer buffer = ByteBuffer.allocateDirect(totalVertices * VxVertexBufferBuilder.STRIDE)
+                .order(ByteOrder.nativeOrder());
+
+        VxVertexBufferBuilder builder = new VxVertexBufferBuilder();
+
+        List<VxDrawCommand> allCommands = new ArrayList<>();
+        Map<String, List<VxDrawCommand>> groupCommands = new HashMap<>();
+
+        int currentVertexOffset = 0;
+
+        // Iterate over groups
+        for (VxRawGroup group : rawModel.groups.values()) {
+            List<VxDrawCommand> thisGroupCommands = new ArrayList<>();
+
+            // Iterate over materials (meshes) within the group
+            for (Map.Entry<String, VxRawMesh> entry : group.meshesByMaterial.entrySet()) {
+                String matName = entry.getKey();
+                VxRawMesh mesh = entry.getValue();
+                int faceCount = mesh.getFaceCount();
+
+                if (faceCount == 0) continue;
+
+                int startVertex = currentVertexOffset;
+                int vertexCount = 0;
+
+                VxMaterial material = rawModel.materials.getOrDefault(matName, new VxMaterial("default"));
+
+                // Iterate over faces using the flat index array
+                for (int i = 0; i < faceCount; i++) {
+                    // Extract indices using the helper methods of VxRawMesh
+                    int v0 = mesh.getPositionIndex(i, 0);
+                    int v1 = mesh.getPositionIndex(i, 1);
+                    int v2 = mesh.getPositionIndex(i, 2);
+
+                    int uv0 = mesh.getUvIndex(i, 0);
+                    int uv1 = mesh.getUvIndex(i, 1);
+                    int uv2 = mesh.getUvIndex(i, 2);
+
+                    int n0 = mesh.getNormalIndex(i, 0);
+                    int n1 = mesh.getNormalIndex(i, 1);
+                    int n2 = mesh.getNormalIndex(i, 2);
+
+                    // Calculate flat normal if vertex normals are missing for this face
+                    Vector3f flatNormal = null;
+                    if (!mesh.hasNormals(i)) {
+                        flatNormal = VxNormalGenerator.calculateFaceNormal(rawModel.positions, v0, v1, v2);
+                    }
+
+                    // Write the 3 vertices of the triangle to the builder
+                    writeVertexToBuilder(builder, rawModel, v0, uv0, n0, material, flatNormal);
+                    writeVertexToBuilder(builder, rawModel, v1, uv1, n1, material, flatNormal);
+                    writeVertexToBuilder(builder, rawModel, v2, uv2, n2, material, flatNormal);
+
+                    vertexCount += 3;
+                }
+
+                // Append to main buffer
+                ByteBuffer segment = builder.buildAndReset();
+                buffer.put(segment);
+
+                // Create Draw Command
+                VxDrawCommand cmd = new VxDrawCommand(material, startVertex, vertexCount);
+                allCommands.add(cmd);
+                thisGroupCommands.add(cmd);
+
+                currentVertexOffset += vertexCount;
+            }
+
+            if (!thisGroupCommands.isEmpty()) {
+                groupCommands.put(group.name, thisGroupCommands);
+            }
+        }
+
+        // Finalize
+        buffer.flip();
+        VxTangentGenerator.calculate(buffer, totalVertices);
+        buffer.rewind();
+
+        return new VxMeshDefinition(buffer, allCommands, groupCommands);
+    }
+
+    private static void writeVertexToBuilder(VxVertexBufferBuilder builder, VxRawModel raw, int pIdx, int uIdx, int nIdx, VxMaterial mat, Vector3f flatNormal) {
+        builder.writeVertex(
+                raw.positions,
+                raw.texCoords,
+                raw.normals,
+                raw.colors,
+                pIdx, uIdx, nIdx,
+                mat.baseColorFactor,
+                flatNormal
+        );
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/model/raw/VxRawMesh.java
@@ -1,0 +1,103 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.model.raw;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+
+/**
+ * Stores the raw face indices for a specific material group using a flat integer array.
+ * <p>
+ * Instead of allocating an object per face (which causes massive GC pressure),
+ * this class packs the 9 indices required for a triangle (3 vertices * (Position + UV + Normal))
+ * into a contiguous {@link IntArrayList}.
+ * <p>
+ * <b>Data Layout per Face (9 integers):</b><br>
+ * [v0, v1, v2, uv0, uv1, uv2, n0, n1, n2]
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxRawMesh {
+
+    /**
+     * The stride size for a single face in the index array.
+     * 3 vertices * 3 index types (Pos, UV, Norm) = 9 integers.
+     */
+    public static final int FACE_STRIDE = 9;
+
+    /**
+     * The flat list of indices.
+     */
+    public final IntArrayList indices = new IntArrayList();
+
+    /**
+     * Adds a single face (triangle) to this mesh.
+     *
+     * @param v0 Index of position vertex 0.
+     * @param v1 Index of position vertex 1.
+     * @param v2 Index of position vertex 2.
+     * @param uv0 Index of texture coord 0 (-1 if none).
+     * @param uv1 Index of texture coord 1 (-1 if none).
+     * @param uv2 Index of texture coord 2 (-1 if none).
+     * @param n0 Index of normal 0 (-1 if none).
+     * @param n1 Index of normal 1 (-1 if none).
+     * @param n2 Index of normal 2 (-1 if none).
+     */
+    public void addFace(int v0, int v1, int v2, int uv0, int uv1, int uv2, int n0, int n1, int n2) {
+        // Positions
+        indices.add(v0);
+        indices.add(v1);
+        indices.add(v2);
+        // UVs
+        indices.add(uv0);
+        indices.add(uv1);
+        indices.add(uv2);
+        // Normals
+        indices.add(n0);
+        indices.add(n1);
+        indices.add(n2);
+    }
+
+    /**
+     * Gets the number of faces (triangles) in this mesh.
+     *
+     * @return The face count.
+     */
+    public int getFaceCount() {
+        return indices.size() / FACE_STRIDE;
+    }
+
+    /**
+     * Checks if a specific face has normal indices defined.
+     *
+     * @param faceIndex The index of the face (0 to count-1).
+     * @return True if normals are present.
+     */
+    public boolean hasNormals(int faceIndex) {
+        int base = faceIndex * FACE_STRIDE;
+        // Check normal indices (offsets 6, 7, 8 in the stride)
+        return indices.getInt(base + 6) != -1;
+    }
+
+    // --- Accessor Methods for Logic/Editing ---
+
+    /**
+     * Retrieves the position index for a specific vertex of a face.
+     *
+     * @param faceIndex The face index.
+     * @param vertexIndex The vertex index within the face (0, 1, or 2).
+     * @return The global position index.
+     */
+    public int getPositionIndex(int faceIndex, int vertexIndex) {
+        return indices.getInt((faceIndex * FACE_STRIDE) + vertexIndex);
+    }
+
+    public int getUvIndex(int faceIndex, int vertexIndex) {
+        return indices.getInt((faceIndex * FACE_STRIDE) + 3 + vertexIndex);
+    }
+
+    public int getNormalIndex(int faceIndex, int vertexIndex) {
+        return indices.getInt((faceIndex * FACE_STRIDE) + 6 + vertexIndex);
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/model/raw/VxRawModel.java
@@ -1,0 +1,75 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.model.raw;
+
+import it.unimi.dsi.fastutil.floats.FloatArrayList;
+import net.xmx.velthoric.renderer.gl.VxMaterial;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Represents the raw, editable data of a 3D model in CPU memory.
+ * <p>
+ * Unlike the final MeshDefinition, this class exposes the structure of the model (vertices,
+ * faces, groups) in a mutable way. This allows for logic-based manipulation or analysis
+ * of the model before it is "baked" into a GPU-ready format.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxRawModel {
+
+    // --- Global Data Pools ---
+    // All groups share these pools to save memory and handle shared vertices.
+    public final FloatArrayList positions = new FloatArrayList();
+    public final FloatArrayList texCoords = new FloatArrayList();
+    public final FloatArrayList normals = new FloatArrayList();
+    public final FloatArrayList colors = new FloatArrayList();
+
+    /**
+     * The structural grouping of the model.
+     * Key: Group Name (e.g., "body", "wheel").
+     */
+    public final Map<String, VxRawGroup> groups = new LinkedHashMap<>();
+
+    /**
+     * A registry of materials used by this model.
+     */
+    public final Map<String, VxMaterial> materials = new LinkedHashMap<>();
+
+    /**
+     * Creates a new empty raw model.
+     */
+    public VxRawModel() {
+        // Default white color fallback
+        colors.add(1.0f); colors.add(1.0f); colors.add(1.0f);
+    }
+
+    /**
+     * Gets or creates a group by name.
+     * @param name The name of the group.
+     * @return The raw group instance.
+     */
+    public VxRawGroup getGroup(String name) {
+        return groups.computeIfAbsent(name, VxRawGroup::new);
+    }
+
+    /**
+     * Calculates the total number of vertices required if this model were to be triangulated and flattened.
+     * Useful for buffer allocation.
+     *
+     * @return The vertex count.
+     */
+    public int calculateTotalVertexCount() {
+        int count = 0;
+        for (VxRawGroup group : groups.values()) {
+            for (VxRawMesh mesh : group.meshesByMaterial.values()) {
+                // Each face has 3 vertices
+                count += mesh.getFaceCount() * 3;
+            }
+        }
+        return count;
+    }
+}

xI-Mx-Ix/VelGFX

common/src/main/java/net/xmx/velgfx/renderer/model/VxModelBaker.java
@@ -1,0 +1,140 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.model;
+
+import net.xmx.velgfx.renderer.gl.VxDrawCommand;
+import net.xmx.velgfx.renderer.gl.VxMaterial;
+import net.xmx.velgfx.renderer.mesh.VxMeshDefinition;
+import net.xmx.velgfx.renderer.model.generator.VxNormalGenerator;
+import net.xmx.velgfx.renderer.model.generator.VxTangentGenerator;
+import net.xmx.velgfx.renderer.model.raw.VxRawGroup;
+import net.xmx.velgfx.renderer.model.raw.VxRawMesh;
+import net.xmx.velgfx.renderer.model.raw.VxRawModel;
+import org.joml.Vector3f;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Responsible for converting a {@link VxRawModel} (CPU-side editable data) into a
+ * {@link VxMeshDefinition} (GPU-ready interleaved buffer).
+ * <p>
+ * This class reads the optimized flat index arrays from {@link VxRawMesh} and processes
+ * them into a stream of vertices for OpenGL.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxModelBaker {
+
+    /**
+     * Bakes the raw model into a renderable mesh definition.
+     *
+     * @param rawModel The raw model data.
+     * @return The GPU-ready mesh definition.
+     */
+    public static VxMeshDefinition bake(VxRawModel rawModel) {
+        int totalVertices = rawModel.calculateTotalVertexCount();
+        if (totalVertices == 0) {
+            return new VxMeshDefinition(ByteBuffer.allocate(0), new ArrayList<>(), new HashMap<>());
+        }
+
+        // Allocate the final buffer
+        ByteBuffer buffer = ByteBuffer.allocateDirect(totalVertices * VxVertexBufferBuilder.STRIDE)
+                .order(ByteOrder.nativeOrder());
+
+        VxVertexBufferBuilder builder = new VxVertexBufferBuilder();
+
+        List<VxDrawCommand> allCommands = new ArrayList<>();
+        Map<String, List<VxDrawCommand>> groupCommands = new HashMap<>();
+
+        int currentVertexOffset = 0;
+
+        // Iterate over groups
+        for (VxRawGroup group : rawModel.groups.values()) {
+            List<VxDrawCommand> thisGroupCommands = new ArrayList<>();
+
+            // Iterate over materials (meshes) within the group
+            for (Map.Entry<String, VxRawMesh> entry : group.meshesByMaterial.entrySet()) {
+                String matName = entry.getKey();
+                VxRawMesh mesh = entry.getValue();
+                int faceCount = mesh.getFaceCount();
+
+                if (faceCount == 0) continue;
+
+                int startVertex = currentVertexOffset;
+                int vertexCount = 0;
+
+                VxMaterial material = rawModel.materials.getOrDefault(matName, new VxMaterial("default"));
+
+                // Iterate over faces using the flat index array
+                for (int i = 0; i < faceCount; i++) {
+                    // Extract indices using the helper methods of VxRawMesh
+                    int v0 = mesh.getPositionIndex(i, 0);
+                    int v1 = mesh.getPositionIndex(i, 1);
+                    int v2 = mesh.getPositionIndex(i, 2);
+
+                    int uv0 = mesh.getUvIndex(i, 0);
+                    int uv1 = mesh.getUvIndex(i, 1);
+                    int uv2 = mesh.getUvIndex(i, 2);
+
+                    int n0 = mesh.getNormalIndex(i, 0);
+                    int n1 = mesh.getNormalIndex(i, 1);
+                    int n2 = mesh.getNormalIndex(i, 2);
+
+                    // Calculate flat normal if vertex normals are missing for this face
+                    Vector3f flatNormal = null;
+                    if (!mesh.hasNormals(i)) {
+                        flatNormal = VxNormalGenerator.calculateFaceNormal(rawModel.positions, v0, v1, v2);
+                    }
+
+                    // Write the 3 vertices of the triangle to the builder
+                    writeVertexToBuilder(builder, rawModel, v0, uv0, n0, material, flatNormal);
+                    writeVertexToBuilder(builder, rawModel, v1, uv1, n1, material, flatNormal);
+                    writeVertexToBuilder(builder, rawModel, v2, uv2, n2, material, flatNormal);
+
+                    vertexCount += 3;
+                }
+
+                // Append to main buffer
+                ByteBuffer segment = builder.buildAndReset();
+                buffer.put(segment);
+
+                // Create Draw Command
+                VxDrawCommand cmd = new VxDrawCommand(material, startVertex, vertexCount);
+                allCommands.add(cmd);
+                thisGroupCommands.add(cmd);
+
+                currentVertexOffset += vertexCount;
+            }
+
+            if (!thisGroupCommands.isEmpty()) {
+                groupCommands.put(group.name, thisGroupCommands);
+            }
+        }
+
+        // Finalize
+        buffer.flip();
+        VxTangentGenerator.calculate(buffer, totalVertices);
+        buffer.rewind();
+
+        return new VxMeshDefinition(buffer, allCommands, groupCommands);
+    }
+
+    private static void writeVertexToBuilder(VxVertexBufferBuilder builder, VxRawModel raw, int pIdx, int uIdx, int nIdx, VxMaterial mat, Vector3f flatNormal) {
+        builder.writeVertex(
+                raw.positions,
+                raw.texCoords,
+                raw.normals,
+                raw.colors,
+                pIdx, uIdx, nIdx,
+                mat.baseColorFactor,
+                flatNormal
+        );
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/model/raw/VxRawMesh.java
@@ -1,0 +1,103 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.model.raw;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+
+/**
+ * Stores the raw face indices for a specific material group using a flat integer array.
+ * <p>
+ * Instead of allocating an object per face (which causes massive GC pressure),
+ * this class packs the 9 indices required for a triangle (3 vertices * (Position + UV + Normal))
+ * into a contiguous {@link IntArrayList}.
+ * <p>
+ * <b>Data Layout per Face (9 integers):</b><br>
+ * [v0, v1, v2, uv0, uv1, uv2, n0, n1, n2]
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxRawMesh {
+
+    /**
+     * The stride size for a single face in the index array.
+     * 3 vertices * 3 index types (Pos, UV, Norm) = 9 integers.
+     */
+    public static final int FACE_STRIDE = 9;
+
+    /**
+     * The flat list of indices.
+     */
+    public final IntArrayList indices = new IntArrayList();
+
+    /**
+     * Adds a single face (triangle) to this mesh.
+     *
+     * @param v0 Index of position vertex 0.
+     * @param v1 Index of position vertex 1.
+     * @param v2 Index of position vertex 2.
+     * @param uv0 Index of texture coord 0 (-1 if none).
+     * @param uv1 Index of texture coord 1 (-1 if none).
+     * @param uv2 Index of texture coord 2 (-1 if none).
+     * @param n0 Index of normal 0 (-1 if none).
+     * @param n1 Index of normal 1 (-1 if none).
+     * @param n2 Index of normal 2 (-1 if none).
+     */
+    public void addFace(int v0, int v1, int v2, int uv0, int uv1, int uv2, int n0, int n1, int n2) {
+        // Positions
+        indices.add(v0);
+        indices.add(v1);
+        indices.add(v2);
+        // UVs
+        indices.add(uv0);
+        indices.add(uv1);
+        indices.add(uv2);
+        // Normals
+        indices.add(n0);
+        indices.add(n1);
+        indices.add(n2);
+    }
+
+    /**
+     * Gets the number of faces (triangles) in this mesh.
+     *
+     * @return The face count.
+     */
+    public int getFaceCount() {
+        return indices.size() / FACE_STRIDE;
+    }
+
+    /**
+     * Checks if a specific face has normal indices defined.
+     *
+     * @param faceIndex The index of the face (0 to count-1).
+     * @return True if normals are present.
+     */
+    public boolean hasNormals(int faceIndex) {
+        int base = faceIndex * FACE_STRIDE;
+        // Check normal indices (offsets 6, 7, 8 in the stride)
+        return indices.getInt(base + 6) != -1;
+    }
+
+    // --- Accessor Methods for Logic/Editing ---
+
+    /**
+     * Retrieves the position index for a specific vertex of a face.
+     *
+     * @param faceIndex The face index.
+     * @param vertexIndex The vertex index within the face (0, 1, or 2).
+     * @return The global position index.
+     */
+    public int getPositionIndex(int faceIndex, int vertexIndex) {
+        return indices.getInt((faceIndex * FACE_STRIDE) + vertexIndex);
+    }
+
+    public int getUvIndex(int faceIndex, int vertexIndex) {
+        return indices.getInt((faceIndex * FACE_STRIDE) + 3 + vertexIndex);
+    }
+
+    public int getNormalIndex(int faceIndex, int vertexIndex) {
+        return indices.getInt((faceIndex * FACE_STRIDE) + 6 + vertexIndex);
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/model/raw/VxRawModel.java
@@ -1,0 +1,75 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.model.raw;
+
+import it.unimi.dsi.fastutil.floats.FloatArrayList;
+import net.xmx.velgfx.renderer.gl.VxMaterial;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Represents the raw, editable data of a 3D model in CPU memory.
+ * <p>
+ * Unlike the final MeshDefinition, this class exposes the structure of the model (vertices,
+ * faces, groups) in a mutable way. This allows for logic-based manipulation or analysis
+ * of the model before it is "baked" into a GPU-ready format.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxRawModel {
+
+    // --- Global Data Pools ---
+    // All groups share these pools to save memory and handle shared vertices.
+    public final FloatArrayList positions = new FloatArrayList();
+    public final FloatArrayList texCoords = new FloatArrayList();
+    public final FloatArrayList normals = new FloatArrayList();
+    public final FloatArrayList colors = new FloatArrayList();
+
+    /**
+     * The structural grouping of the model.
+     * Key: Group Name (e.g., "body", "wheel").
+     */
+    public final Map<String, VxRawGroup> groups = new LinkedHashMap<>();
+
+    /**
+     * A registry of materials used by this model.
+     */
+    public final Map<String, VxMaterial> materials = new LinkedHashMap<>();
+
+    /**
+     * Creates a new empty raw model.
+     */
+    public VxRawModel() {
+        // Default white color fallback
+        colors.add(1.0f); colors.add(1.0f); colors.add(1.0f);
+    }
+
+    /**
+     * Gets or creates a group by name.
+     * @param name The name of the group.
+     * @return The raw group instance.
+     */
+    public VxRawGroup getGroup(String name) {
+        return groups.computeIfAbsent(name, VxRawGroup::new);
+    }
+
+    /**
+     * Calculates the total number of vertices required if this model were to be triangulated and flattened.
+     * Useful for buffer allocation.
+     *
+     * @return The vertex count.
+     */
+    public int calculateTotalVertexCount() {
+        int count = 0;
+        for (VxRawGroup group : groups.values()) {
+            for (VxRawMesh mesh : group.meshesByMaterial.values()) {
+                // Each face has 3 vertices
+                count += mesh.getFaceCount() * 3;
+            }
+        }
+        return count;
+    }
+}