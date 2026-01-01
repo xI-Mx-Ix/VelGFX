xI-Mx-Ix/Velthoric

refactor: Restructure rendering system and isolate OpenGL logic into dedicated package

vx-renderer/src/main/java/net/xmx/velthoric/renderer/gl/VxDrawCommand.java
@@ -2,7 +2,7 @@
  * This file is part of Velthoric.
  * Licensed under LGPL 3.0.
  */
-package net.xmx.velthoric.renderer;
+package net.xmx.velthoric.renderer.gl;
 
 /**
  * Represents a single, self-contained draw call for a portion of a mesh.

vx-renderer/src/main/java/net/xmx/velthoric/renderer/gl/VxMaterial.java
@@ -2,9 +2,10 @@
  * This file is part of Velthoric.
  * Licensed under LGPL 3.0.
  */
-package net.xmx.velthoric.renderer;
+package net.xmx.velthoric.renderer.gl;
 
 import net.minecraft.resources.ResourceLocation;
+import net.xmx.velthoric.renderer.VxRConstants;
 
 /**
  * Represents a PBR material with properties loaded from an MTL file.

vx-renderer/src/main/java/net/xmx/velthoric/renderer/mesh/VxAbstractRenderableMesh.java
@@ -10,7 +10,7 @@ import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
-import net.xmx.velthoric.renderer.VxDrawCommand;
+import net.xmx.velthoric.renderer.gl.VxDrawCommand;
 
 import java.util.Collections;
 import java.util.List;

vx-renderer/src/main/java/net/xmx/velthoric/renderer/mesh/impl/VxDedicatedMesh.java
@@ -1,0 +1,77 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.renderer.mesh.impl;
+
+import net.xmx.velthoric.renderer.gl.VxDrawCommand;
+import net.xmx.velthoric.renderer.gl.VxVertexBuffer;
+import net.xmx.velthoric.renderer.mesh.VxAbstractRenderableMesh;
+import net.xmx.velthoric.renderer.mesh.VxMeshDefinition;
+
+import java.nio.ByteBuffer;
+
+/**
+ * A mesh that possesses its own dedicated Vertex Buffer Object (VBO).
+ * <p>
+ * This class replaces the old StandaloneMesh and utilizes the new {@link VxVertexBuffer} wrapper.
+ * It is useful for large or frequently changing meshes that shouldn't share
+ * the arena buffer, or when strict isolation is required.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxDedicatedMesh extends VxAbstractRenderableMesh {
+
+    /**
+     * The dedicated vertex buffer instance containing the mesh data.
+     */
+    private final VxVertexBuffer vertexBuffer;
+
+    /**
+     * Constructs a dedicated mesh from a generic mesh definition.
+     * Automatically uploads vertex data into a new VBO.
+     *
+     * @param definition The mesh definition containing data and structure.
+     */
+    public VxDedicatedMesh(VxMeshDefinition definition) {
+        super(definition.allDrawCommands, definition.getGroupDrawCommands());
+
+        ByteBuffer data = definition.getVertexData();
+        // Create a dedicated buffer for this mesh with STATIC_DRAW usage (false for dynamic).
+        this.vertexBuffer = new VxVertexBuffer(data.remaining(), false);
+        this.vertexBuffer.uploadSubData(0, data);
+
+        initializeTextures();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Binds the unique VBO/VAO associated with this mesh via the wrapper.
+     */
+    @Override
+    public void setupVaoState() {
+        this.vertexBuffer.bind();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Since this mesh owns the buffer starting at index 0, the final offset is just the command's local offset.
+     */
+    @Override
+    public int getFinalVertexOffset(VxDrawCommand command) {
+        return command.vertexOffset;
+    }
+
+    /**
+     * Deletes the dedicated GPU resources associated with this mesh.
+     */
+    @Override
+    public void delete() {
+        if (!isDeleted) {
+            this.vertexBuffer.delete();
+            isDeleted = true;
+        }
+    }
+}

vx-renderer/src/main/java/net/xmx/velthoric/renderer/model/VxModelManager.java
@@ -8,9 +8,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.xmx.velthoric.renderer.VxRConstants;
 import net.xmx.velthoric.renderer.mesh.IVxRenderableMesh;
 import net.xmx.velthoric.renderer.mesh.VxMeshDefinition;
-import net.xmx.velthoric.renderer.mesh.standalone.VxStandaloneMesh;
 import net.xmx.velthoric.renderer.mesh.arena.VxArenaBuffer;
-import net.xmx.velthoric.renderer.mesh.arena.VxArenaMesh;
+import net.xmx.velthoric.renderer.mesh.impl.VxArenaMesh;
+import net.xmx.velthoric.renderer.mesh.impl.VxDedicatedMesh;
 import net.xmx.velthoric.renderer.model.parser.VxObjParser;
 
 import java.io.IOException;
@@ -36,7 +36,7 @@ public final class VxModelManager {
     /**
      * Caches GPU-side standalone meshes, each with its own VBO/VAO.
      */
-    private static final Map<ResourceLocation, VxStandaloneMesh> STANDALONE_MESH_CACHE = new HashMap<>();
+    private static final Map<ResourceLocation, VxDedicatedMesh> STANDALONE_MESH_CACHE = new HashMap<>();
 
     /**
      * Caches handles to sub-meshes allocated within the global arena buffer.
@@ -53,17 +53,17 @@ public final class VxModelManager {
      * Standalone meshes are self-contained and manage their own GPU resources.
      *
      * @param location The resource location of the model file.
-     * @return An Optional containing the {@link VxStandaloneMesh}, or empty if loading fails.
+     * @return An Optional containing the {@link VxDedicatedMesh}, or empty if loading fails.
      */
-    public static Optional<VxStandaloneMesh> getStandaloneMesh(ResourceLocation location) {
-        VxStandaloneMesh cached = STANDALONE_MESH_CACHE.get(location);
+    public static Optional<VxDedicatedMesh> getStandaloneMesh(ResourceLocation location) {
+        VxDedicatedMesh cached = STANDALONE_MESH_CACHE.get(location);
         if (cached != null) {
             return Optional.of(cached);
         }
 
         Optional<VxMeshDefinition> definitionOpt = getDefinition(location);
         if (definitionOpt.isPresent()) {
-            VxStandaloneMesh gpuMesh = new VxStandaloneMesh(definitionOpt.get());
+            VxDedicatedMesh gpuMesh = new VxDedicatedMesh(definitionOpt.get());
             STANDALONE_MESH_CACHE.put(location, gpuMesh);
             return Optional.of(gpuMesh);
         } else {

vx-renderer/src/main/java/net/xmx/velthoric/renderer/model/parser/VxObjParser.java
@@ -7,8 +7,8 @@ package net.xmx.velthoric.renderer.model.parser;
 import it.unimi.dsi.fastutil.floats.FloatArrayList;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
-import net.xmx.velthoric.renderer.VxDrawCommand;
-import net.xmx.velthoric.renderer.VxMaterial;
+import net.xmx.velthoric.renderer.gl.VxDrawCommand;
+import net.xmx.velthoric.renderer.gl.VxMaterial;
 import net.xmx.velthoric.renderer.VxRConstants;
 import net.xmx.velthoric.renderer.mesh.VxMeshDefinition;
 import net.xmx.velthoric.renderer.model.VxVertexBufferBuilder;

vx-renderer/src/main/java/net/xmx/velthoric/renderer/util/VxShaderDetector.java
@@ -2,7 +2,9 @@
  * This file is part of Velthoric.
  * Licensed under LGPL 3.0.
  */
-package net.xmx.velthoric.renderer;
+package net.xmx.velthoric.renderer.util;
+
+import net.xmx.velthoric.renderer.VxRConstants;
 
 import java.lang.reflect.Method;
 

xI-Mx-Ix/VelGFX

common/src/main/java/net/xmx/velgfx/renderer/gl/VxDrawCommand.java
@@ -2,7 +2,7 @@
  * This file is part of VelGFX.
  * Licensed under LGPL 3.0.
  */
-package net.xmx.velgfx.renderer;
+package net.xmx.velgfx.renderer.gl;
 
 /**
  * Represents a single, self-contained draw call for a portion of a mesh.

common/src/main/java/net/xmx/velgfx/renderer/gl/VxMaterial.java
@@ -2,9 +2,10 @@
  * This file is part of VelGFX.
  * Licensed under LGPL 3.0.
  */
-package net.xmx.velgfx.renderer;
+package net.xmx.velgfx.renderer.gl;
 
 import net.minecraft.resources.ResourceLocation;
+import net.xmx.velgfx.renderer.VelGFX;
 
 /**
  * Represents a PBR material with properties loaded from an MTL file.

common/src/main/java/net/xmx/velgfx/renderer/mesh/VxAbstractRenderableMesh.java
@@ -10,7 +10,7 @@ import net.minecraft.client.renderer.texture.AbstractTexture;
 import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.resources.ResourceLocation;
-import net.xmx.velgfx.renderer.VxDrawCommand;
+import net.xmx.velgfx.renderer.gl.VxDrawCommand;
 
 import java.util.Collections;
 import java.util.List;

common/src/main/java/net/xmx/velgfx/renderer/mesh/impl/VxDedicatedMesh.java
@@ -1,0 +1,77 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.renderer.mesh.impl;
+
+import net.xmx.velgfx.renderer.gl.VxDrawCommand;
+import net.xmx.velgfx.renderer.gl.VxVertexBuffer;
+import net.xmx.velgfx.renderer.mesh.VxAbstractRenderableMesh;
+import net.xmx.velgfx.renderer.mesh.VxMeshDefinition;
+
+import java.nio.ByteBuffer;
+
+/**
+ * A mesh that possesses its own dedicated Vertex Buffer Object (VBO).
+ * <p>
+ * This class replaces the old StandaloneMesh and utilizes the new {@link VxVertexBuffer} wrapper.
+ * It is useful for large or frequently changing meshes that shouldn't share
+ * the arena buffer, or when strict isolation is required.
+ *
+ * @author xI-Mx-Ix
+ */
+public class VxDedicatedMesh extends VxAbstractRenderableMesh {
+
+    /**
+     * The dedicated vertex buffer instance containing the mesh data.
+     */
+    private final VxVertexBuffer vertexBuffer;
+
+    /**
+     * Constructs a dedicated mesh from a generic mesh definition.
+     * Automatically uploads vertex data into a new VBO.
+     *
+     * @param definition The mesh definition containing data and structure.
+     */
+    public VxDedicatedMesh(VxMeshDefinition definition) {
+        super(definition.allDrawCommands, definition.getGroupDrawCommands());
+
+        ByteBuffer data = definition.getVertexData();
+        // Create a dedicated buffer for this mesh with STATIC_DRAW usage (false for dynamic).
+        this.vertexBuffer = new VxVertexBuffer(data.remaining(), false);
+        this.vertexBuffer.uploadSubData(0, data);
+
+        initializeTextures();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Binds the unique VBO/VAO associated with this mesh via the wrapper.
+     */
+    @Override
+    public void setupVaoState() {
+        this.vertexBuffer.bind();
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Since this mesh owns the buffer starting at index 0, the final offset is just the command's local offset.
+     */
+    @Override
+    public int getFinalVertexOffset(VxDrawCommand command) {
+        return command.vertexOffset;
+    }
+
+    /**
+     * Deletes the dedicated GPU resources associated with this mesh.
+     */
+    @Override
+    public void delete() {
+        if (!isDeleted) {
+            this.vertexBuffer.delete();
+            isDeleted = true;
+        }
+    }
+}

common/src/main/java/net/xmx/velgfx/renderer/model/VxModelManager.java
@@ -8,9 +8,9 @@ import net.minecraft.resources.ResourceLocation;
 import net.xmx.velgfx.renderer.VelGFX;
 import net.xmx.velgfx.renderer.mesh.IVxRenderableMesh;
 import net.xmx.velgfx.renderer.mesh.VxMeshDefinition;
-import net.xmx.velgfx.renderer.mesh.standalone.VxStandaloneMesh;
 import net.xmx.velgfx.renderer.mesh.arena.VxArenaBuffer;
-import net.xmx.velgfx.renderer.mesh.arena.VxArenaMesh;
+import net.xmx.velgfx.renderer.mesh.impl.VxArenaMesh;
+import net.xmx.velgfx.renderer.mesh.impl.VxDedicatedMesh;
 import net.xmx.velgfx.renderer.model.parser.VxObjParser;
 
 import java.io.IOException;
@@ -36,7 +36,7 @@ public final class VxModelManager {
     /**
      * Caches GPU-side standalone meshes, each with its own VBO/VAO.
      */
-    private static final Map<ResourceLocation, VxStandaloneMesh> STANDALONE_MESH_CACHE = new HashMap<>();
+    private static final Map<ResourceLocation, VxDedicatedMesh> STANDALONE_MESH_CACHE = new HashMap<>();
 
     /**
      * Caches handles to sub-meshes allocated within the global arena buffer.
@@ -53,17 +53,17 @@ public final class VxModelManager {
      * Standalone meshes are self-contained and manage their own GPU resources.
      *
      * @param location The resource location of the model file.
-     * @return An Optional containing the {@link VxStandaloneMesh}, or empty if loading fails.
+     * @return An Optional containing the {@link VxDedicatedMesh}, or empty if loading fails.
      */
-    public static Optional<VxStandaloneMesh> getStandaloneMesh(ResourceLocation location) {
-        VxStandaloneMesh cached = STANDALONE_MESH_CACHE.get(location);
+    public static Optional<VxDedicatedMesh> getStandaloneMesh(ResourceLocation location) {
+        VxDedicatedMesh cached = STANDALONE_MESH_CACHE.get(location);
         if (cached != null) {
             return Optional.of(cached);
         }
 
         Optional<VxMeshDefinition> definitionOpt = getDefinition(location);
         if (definitionOpt.isPresent()) {
-            VxStandaloneMesh gpuMesh = new VxStandaloneMesh(definitionOpt.get());
+            VxDedicatedMesh gpuMesh = new VxDedicatedMesh(definitionOpt.get());
             STANDALONE_MESH_CACHE.put(location, gpuMesh);
             return Optional.of(gpuMesh);
         } else {

common/src/main/java/net/xmx/velgfx/renderer/model/parser/VxObjParser.java
@@ -7,8 +7,8 @@ package net.xmx.velgfx.renderer.model.parser;
 import it.unimi.dsi.fastutil.floats.FloatArrayList;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
-import net.xmx.velgfx.renderer.VxDrawCommand;
-import net.xmx.velgfx.renderer.VxMaterial;
+import net.xmx.velgfx.renderer.gl.VxDrawCommand;
+import net.xmx.velgfx.renderer.gl.VxMaterial;
 import net.xmx.velgfx.renderer.VelGFX;
 import net.xmx.velgfx.renderer.mesh.VxMeshDefinition;
 import net.xmx.velgfx.renderer.model.VxVertexBufferBuilder;

common/src/main/java/net/xmx/velgfx/renderer/util/VxShaderDetector.java
@@ -2,7 +2,9 @@
  * This file is part of VelGFX.
  * Licensed under LGPL 3.0.
  */
-package net.xmx.velgfx.renderer;
+package net.xmx.velgfx.renderer.util;
+
+import net.xmx.velgfx.renderer.VelGFX;
 
 import java.lang.reflect.Method;
 