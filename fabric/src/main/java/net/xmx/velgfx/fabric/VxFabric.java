af892fb3497c6a1343792e4f927610ea4b4fab6b
xI-Mx-Ix <xmxv.wz@gmail.com>
2026-01-01 11:51:30 +0100

    Initial commit

---
+++ fabric/src/main/java/net/xmx/velgfx/fabric/VxFabric.java
@@ -0,0 +1,19 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * Main class for Fabric integration.
+ *
+ * @author xI-Mx-Ix
+ */
+public final class VxFabric implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ neoforge/src/main/java/net/xmx/velgfx/neoforge/VxNeoForge.java
@@ -0,0 +1,18 @@
+/*
+ * This file is part of VelGFX.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velgfx.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+/**
+ * Main class for NeoForge integration.
+ *
+ * @author xI-Mx-Ix
+ */
+@Mod("velgfx")
+public final class VxNeoForge {
+    public VxNeoForge() {
+    }
+}
